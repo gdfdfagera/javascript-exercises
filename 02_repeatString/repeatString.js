30050eb78f06e8771b0473802fa7c4650a1247db
Disa <dias.gainatulinsc@gmail.com>
2023-07-18 14:19:37 +0600

    2_ex

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (times < 0){
+        return "ERROR";
+    }
+    var w = '';
+    for (var i = 0; i < times; i++){
+        w += word;
+    }
 
+    return w;
 };
 
 // Do not edit below this line
