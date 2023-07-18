612e565dd30bd4b755a07aca3c414a7a3ab1f4a5
Disa <dias.gainatulinsc@gmail.com>
2023-07-18 14:45:52 +0600

    finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var a = [...string];
+    var t = '';
+    for (var i = 0; i < string.length; i++){
+        t += a[string.length - i - 1];
+    }
 
+    return t;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    var d = 0;
+    for (var i = a; i <= b; i++){
+        d += i;
+    }
+    return d;
 };
 
 // Do not edit below this line
