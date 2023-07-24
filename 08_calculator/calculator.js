d53d372bc4caa1a743f116b35ed516f2e7dc489d
Disa <dias.gainatulinsc@gmail.com>
2023-07-24 15:10:09 +0600

    finishedAll

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function([...a]) {
+  let t = 0;
+	a.forEach(c => {
+    t += c;
+  });
+  return c;
 };
 
-const multiply = function() {
-
+const multiply = function([...a]) {
+  let t = 1;
+	a.forEach(c => {
+    t *= c;
+  });
+  return c;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let t = 1;
+	for (let i = 0; i < b; i++){
+    t *= a;
+  }
+  return t;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0){
+    return 1;
+  }
+
+  let t = 1;
+  for (let i = 1; i <= a; i++){
+    t *= i;
+  }
+  return t;
 };
 
 // Do not edit below this line
